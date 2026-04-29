7fcd75acc5dfc0a76c674f03cb87859f852198a7
pulgalletita <contactojimenezr@gmail.com>
2026-04-28 23:48:46 -0400

    Update UsuarioController.php

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -1,1 +1,13 @@
+<?php
+require_once 'app/models/Usuario.php';
 
+class UsuarioController {
+
+    public function guardar() {
+        $usuario = new Usuario();
+        $usuario->nombre = $_POST['usuario'];
+        $usuario->correo = $_POST['correo'];
+
+        return $usuario->guardar();
+    }
+}
