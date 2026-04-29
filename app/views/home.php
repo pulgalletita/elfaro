a506ac949ec7a4a9d3d2f62c4712294f7937be06
pulgalletita <contactojimenezr@gmail.com>
2026-04-28 23:48:03 -0400

    Update home.php

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,6 @@
-
+<form method="POST">
+    <input type="text" name="usuario" placeholder="Usuario">
+    <input type="email" name="correo" placeholder="Correo">
+    <input type="password" name="password" placeholder="Contraseña">
+    <button type="submit">Registrarse</button>
+</form>
